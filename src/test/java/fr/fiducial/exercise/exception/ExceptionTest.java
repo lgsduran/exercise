4b2b010b9664f2bf4e166b7abe3e6f9c00d55b5e
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 <lgsduran@yahoo.com.br>
2023-07-01 17:14:17 +0100

    pkg changed

--- src/test/java/fr/fiducial/exercise/exception/ExceptionTest.java
+++ src/test/java/fr/fiducial/exercise/exception/ExceptionTest.java
@@ -1,4 +1,4 @@
-package fr.fiducial.exercise.service.exception;
+package fr.fiducial.exercise.exception;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
