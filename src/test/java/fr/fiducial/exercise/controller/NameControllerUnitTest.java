40cbea179b65f68f522d02b64afe9f3858be6e43
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 <lgsduran@yahoo.com.br>
2024-04-19 13:39:06 -0300

    added test

--- src/test/java/fr/fiducial/exercise/controller/NameControllerUnitTest.java
+++ src/test/java/fr/fiducial/exercise/controller/NameControllerUnitTest.java
@@ -4,7 +4,9 @@ import static java.time.Instant.now;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,7 +25,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -34,7 +35,6 @@ import fr.fiducial.exercise.service.NamesServiceImpl;
 
 @WebMvcTest(controllers = NameController.class)
 @ExtendWith(SpringExtension.class)
-//@ExtendWith(SpringExtension.class)
 public class NameControllerUnitTest {
 
 	@Autowired
@@ -42,70 +42,86 @@ public class NameControllerUnitTest {
 
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@MockBean
 	private NamesServiceImpl namesService;
-	
+
 	private ArrayList<NamesDto> namesList = new ArrayList<>();
 	private NamesDto namesDto;
-	
-    @BeforeEach
-    public void setUp() {
-    	namesDto = new NamesDto(2L, "Lebron James", now());
-    	namesList.add(namesDto);       
-    }
+
+	@BeforeEach
+	public void setUp() {
+		namesDto = new NamesDto(1L, "Lebron James", now());
+		namesList.add(namesDto);
+	}
 
 	@Test
 	@DisplayName("Should save nameDto")
 	void NamesController_saveName_ReturnNamesDto() throws Exception {	
 		when(this.namesService.save(any())).thenReturn(namesDto);
 
-		ResultActions response = mockMvc.perform(post("/api/names/addName").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(namesDto)));
-
-		response.andExpect(status().is2xxSuccessful())
+		mockMvc.perform(post("/api/names/addName").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(namesDto)))
+				.andExpect(status().is2xxSuccessful())
 				.andExpect(jsonPath("$.id", is(longToIntCast(namesDto.getId()))))
 				.andExpect(jsonPath("$.name", is(namesDto.getName())))
 				.andExpect(jsonPath("$.created_At", notNullValue()));
 	}
-	
+
 	@Test
 	@DisplayName("Should saveAll namesDto")
 	void NamesController_saveAllName_ReturnNamesDto() throws Exception {
 		when(this.namesService.saveAll(any())).thenReturn(namesList);
 
-		ResultActions response = mockMvc.perform(post("/api/names/addNames")
+		mockMvc.perform(post("/api/names/addNames")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(namesList)));
-
-		response.andExpect(status().is2xxSuccessful())				
+				.content(objectMapper.writeValueAsString(namesList)))
+				.andExpect(status().is2xxSuccessful())				
 				.andExpect(jsonPath("$.[0].name", is(namesList.get(0).getName())));
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Test
 	@DisplayName("Should retrieve all namesDto")
 	void NamesController_listNames_ReturnNames() throws Exception {
 		Page<Names> pagedResponse = new PageImpl(namesList);
 		when(this.namesService.listNames(any())).thenReturn(pagedResponse);
-		
-		ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/names/")
-				.contentType(MediaType.APPLICATION_JSON));
-		
-		response.andExpect(status().is2xxSuccessful())				
-			.andExpect(jsonPath("$.content.[0].name", is(namesList.get(0).getName())));			
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/names/").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(jsonPath("$.content.[0].name", is(namesList.get(0).getName())));
 	}
-	
+
 	@Test
 	@DisplayName("Should verify that name exists")
 	void NamesController_NameExists_ReturnTrue() throws Exception {
 		when(this.namesService.nameExists(any())).thenReturn(true);
 		
-		ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/names/Lebron James")
-				.contentType(MediaType.APPLICATION_JSON));
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/names/Lebron James")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is2xxSuccessful())				
+				.andExpect(jsonPath("$", is(true)));
+	}
 
-		response.andExpect(status().is2xxSuccessful())				
-			.andExpect(jsonPath("$", is(true)));
+	@Test
+	@DisplayName("Should verify that name does not exist")
+	void NamesController_NameExists_ReturnFalse() throws Exception {
+		when(this.namesService.nameExists(any())).thenReturn(false);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/names/Lebron James")
+				.contentType(MediaType.APPLICATION_JSON))	
+				.andExpect(status().is2xxSuccessful())				
+				.andExpect(jsonPath("$", is(false)));
+	}
+
+	@Test
+	@DisplayName("Should delete name")
+	void NameController_DeleteName_ReturnOk() throws Exception {
+		doNothing().when(this.namesService).deleteByName(namesDto.getName());
+
+		mockMvc.perform(delete("/api/names/Lebron James")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is2xxSuccessful());
 	}
 
 	private int longToIntCast(long number) {
