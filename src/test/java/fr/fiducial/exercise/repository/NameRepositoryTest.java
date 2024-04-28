44ff395e77f6c8f110b45c3ab831df948361099e
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 <lgsduran@yahoo.com.br>
2024-04-28 19:38:31 -0300

    added repository test

---
+++ src/test/java/fr/fiducial/exercise/repository/NameRepositoryTest.java
@@ -0,0 +1,89 @@
+package fr.fiducial.exercise.repository;
+
+import static java.time.Instant.now;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import fr.fiducial.exercise.entity.Names;
+
+@SpringBootTest
+@Testcontainers
+@TestMethodOrder(OrderAnnotation.class)
+public class NameRepositoryTest {
+
+	@Autowired
+	private NamesRepository namesRepository;
+
+	@Container
+	@ServiceConnection
+	static MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:latest");
+
+	@Test
+	@Order(1)
+	@DisplayName("Should verify that id exists")
+	public void testSaveNameAndFindById() {
+		Names lebron = new Names("Lebron James", now());
+		namesRepository.save(lebron);
+		Names resultName = namesRepository.findById(lebron.getId()).get();
+		assertThat(resultName).isNotNull();
+		assertThat(resultName).extracting(Names::getId).isNotNull();
+		assertThat(resultName).extracting(Names::getCreatedAt).isNotNull();
+		assertThat(resultName.getName()).isNotNull().matches(lebron.getName()::equalsIgnoreCase);
+	}
+
+	@Test
+	@Order(2)
+	@DisplayName("Should verify that name exists")
+	public void testFindByName() {
+		Names lebron = new Names("Mike Tyson", now());
+		namesRepository.save(lebron);
+		Names resultName = namesRepository.findByName(lebron.getName());
+		assertThat(resultName.getName()).isNotNull().matches(lebron.getName()::equalsIgnoreCase);
+	}
+
+	@Test
+	@Transactional
+	@Order(3)
+	@DisplayName("Should delete by name")
+	public void testDeleteByName() {
+		Names lebron = new Names("Lebron James", now());
+		namesRepository.save(lebron);
+		namesRepository.deleteByName(lebron.getName().toLowerCase());
+		var finalResult = namesRepository.findById(lebron.getId());
+		assertThat(finalResult.isEmpty()).isTrue();
+	}
+
+	@Test
+	@Order(4)
+	@DisplayName("Should delete all by ids")
+	public void testDeleteByIds() {
+		var namesList = new ArrayList<Names>();
+		namesList.add(new Names("Lionel Messi", now()));
+		namesList.add(new Names("Michael Jordan", now()));
+
+		namesRepository.saveAll(namesList);
+
+		List<Names> all = namesRepository.findAll();
+		var ids = new ArrayList<Long>();
+		all.forEach(id -> ids.add(id.getId()));
+		namesRepository.deleteAllById(ids);
+
+		var result = namesRepository.findAllById(ids).stream().findAny();
+		assertThat(result.isPresent()).isFalse();
+	}
+}
