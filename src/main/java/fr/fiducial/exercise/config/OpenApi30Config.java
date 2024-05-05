239a069faee54b5f698837af8ae448a8479dab0d
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 <lgsduran@yahoo.com.br>
2024-05-05 19:12:12 -0300

    test

--- src/main/java/fr/fiducial/exercise/config/OpenApi30Config.java
+++ src/main/java/fr/fiducial/exercise/config/OpenApi30Config.java
@@ -1,10 +1,8 @@
 package fr.fiducial.exercise.config;
 
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@Configuration
+//@Configuration
 @OpenAPIDefinition(info = @Info(title = "Exercise", version = "v1"))
 public class OpenApi30Config {}
